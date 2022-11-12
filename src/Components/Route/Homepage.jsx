e4a9fc61c18e5e462960fe5aa617891df518c6b5
vijay-kumar-yadav <vky405021@gmail.com>
2022-11-12 17:51:01 +0530

    navbar basic added without functionality

--- src/Components/Route/Homepage.jsx
+++ src/Components/Route/Homepage.jsx
@@ -4,21 +4,26 @@ import Navbar from "../Parts/Navbar";
 
 
 const Homepage = () => {
+
     let questionTab = Array.from({ length: 20 }, (_, i) => i + 1)
     console.log(questionTab)
     return (
         <>
             <Navbar />
-            <div className="container w-75 ">
-                {
-                    questionTab.splice(0, 10).map((i, j) =>
+            <div className="container" >
 
-                        <Content key={j} />
+                <h1 style={{ marginTop: "80px" }} className="mb-3 text-start">Questions</h1>
+                <div className="container">
+                    {
+                        questionTab.splice(0, 10).map((i, j) =>
+
+                            <Content key={j} />
 
 
-                    )
+                        )
 
-                }
+                    }
+                </div>
             </div>
         </>
     )
